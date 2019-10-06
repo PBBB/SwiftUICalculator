fbb8ca7388e1aca5d0bbdbfa808471ac17ab7408
Issac Penn <pbbsignup@outlook.com>
2019-10-06 23:11:10 +0800

    Initial Commit

---
+++ SwiftUICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by Issac Penn on 2019/10/6.
+//  Copyright © 2019 Issac Penn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
